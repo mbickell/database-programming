610e80f35ded886a4cd198d9595ddd5daaf760ee
Matthew Bickell <matthewbickell@hotmail.com>
2026-02-17 11:41:55 +0200

    feat: connect to db

---
+++ connector.py
@@ -0,0 +1,31 @@
+import os
+import mariadb
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# connection parameters
+conn_params= {
+    "user" : os.getenv("MARIA_DB_USER"),
+    "password" : os.getenv("MARIA_DB_PASSWORD"),
+    "host" : os.getenv("MARIA_DB_HOST"),
+    "database" : "e2501254_test"
+}
+
+# Establish a connection
+connection= mariadb.connect(**conn_params)
+
+cursor= connection.cursor()
+
+# Populate countries table  with some data
+
+# retrieve data
+cursor.execute("SELECT name, code, location FROM property")
+
+# print content
+row= cursor.fetchone()
+print(*row, sep=' ')
+
+# free resources
+cursor.close()
+connection.close()
