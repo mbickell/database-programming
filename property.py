681a1730c2f54f778cf8bd8329284b96e41a6bb9
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-05 14:02:43 +0200

    feat: add delete functionality

--- property.py
+++ property.py
@@ -11,14 +11,17 @@ def begin_cmd(cursor, connection, mariadb):
       case "a":
         property = request_property()
         add_property(cursor, property)
-        commit_property(connection, property)
+        commit_property_add(connection, property)
       case "d":
-        print("Recived D")
+        id = delete_property()
+        commit_property_deletion(cursor, mariadb, connection, id)
       case "x":
         print("Exiting...")
         break
       case _:
         print(command)
+
+    print()
 
 def list_propeties(cursor, mariadb):
   try:
@@ -57,7 +60,7 @@ def request_property():
 
   return property
 
-def commit_property(connection, property):
+def commit_property_add(connection, property):
   
   while (1):
     print(f"Property with values - code: {property["code"]}, name: {property["name"]}, location: {property["location"]}")
@@ -76,3 +79,41 @@ def commit_property(connection, property):
         break
       case _:
         print("Command not recognised")
+
+def commit_property_deletion(cursor, mariadb, connection, id):
+  data = select_property_with_id(cursor, mariadb, id)
+
+  while (1):
+    print(f"Property: {data}")
+    print("Is ready to be deleted, please press [Y] to commit or [N] to rollback")
+
+    user_input = input().lower()
+
+    match user_input:
+      case "y":
+        print("Committing...")
+        cursor.execute(f"DELETE FROM property WHERE id = {id}")
+        connection.commit()
+        break
+      case "n":
+        print("Rolling back...")
+        break
+      case _:
+        print("Command not recognised")
+
+def delete_property():
+  print("Enter the id of the property to be deleted")
+  
+  user_input = input().strip()
+
+  return user_input
+
+def select_property_with_id(cursor, mariadb, id):
+  try:
+    cursor.execute(f"SELECT * FROM property WHERE id = {id}")
+  except mariadb.Error as e:
+    print(f"Error: {e}")
+
+  data = cursor.fetchone()
+
+  return data
