180081a67c18bea7b9d0a2f5b3d942478e4b0027
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-12 14:14:09 +0200

    feat: insert property and check its id

--- crud/property/read.py
+++ crud/property/read.py
@@ -13,11 +13,11 @@ def print_properties(cursor, mariadb):
   for datum in data:
     print(f"id: {datum[0]}, code: {datum[1]}, name: {datum[2]}, location: {datum[3]}")
 
-def check_property_exists(cursor, mariadb, property_code):
+def get_property(cursor, mariadb, property_code):
   try:
     cursor.execute(f"SELECT * FROM property WHERE code=\"{property_code}\"")
   except mariadb.Error as e:
     print(f"Error: {e}")
 
   data = cursor.fetchall()
-  return len(data) > 0
+  return data
--- terminal/cli.py
+++ terminal/cli.py
@@ -1,7 +1,7 @@
 import argparse
 from terminal.property import begin_property_cli 
 from terminal.electricty import get_property_code
-from crud.property.read import check_property_exists
+from crud.property.read import get_property
 from crud.property.add import add_property
 from crud.electricity.add import read_json_data
 
@@ -13,7 +13,8 @@ def begin_cmd(cursor, connection, mariadb):
   
   if (args.file):
     code = get_property_code(args.file)
-    exists = check_property_exists(cursor, mariadb, code)
+    exists = len(get_property(cursor, mariadb, code)) > 0
+    print(exists)
     if exists:
       print("Property already exists")
     else:
@@ -21,6 +22,8 @@ def begin_cmd(cursor, connection, mariadb):
       location_name = electricity_data[0]["locationName"]
       print({"code": code, "name": location_name, "location": location_name})
       # add_property(cursor, mariadb, {"code": code, "name": location_name, "location": location_name})
+      property_id = get_property(cursor, mariadb, code)[0]["id"]
+      # Insert the electricity data
       return
   else:
     begin_property_cli(cursor, connection, mariadb)
