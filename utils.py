820e268d1dfc389bf06f854065da1e42c3186b91
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-20 10:23:20 +0200

    feat: add a util for cursor execute

---
+++ utils.py
@@ -0,0 +1,8 @@
+import mariadb
+from connector import cursor
+
+def execute(sql_statement, values):
+  try:
+    cursor.execute(sql_statement, **values)
+  except mariadb.Error as e:
+    print(f"Error: {e}")
