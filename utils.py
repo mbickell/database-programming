d0d7773362a6f5dff8fdcf295461130763e5dc6f
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-20 10:25:58 +0200

    fix: util function

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ from connector import cursor
 
 def execute(sql_statement, values):
   try:
-    cursor.execute(sql_statement, **values)
+    cursor.execute(sql_statement, values)
   except mariadb.Error as e:
     print(f"Error: {e}")
