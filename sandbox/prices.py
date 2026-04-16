8b730d4296bb687ec7b1ba518c89e8255d8af6ac
Matthew Bickell <matthewbickell@hotmail.com>
2026-04-16 08:46:33 +0300

    fix: remove duplicated code

--- sandbox/prices.py
+++ sandbox/prices.py
@@ -1,30 +1,7 @@
 import csv
-import os
-import sys
-from dotenv import load_dotenv
-import mariadb
 from datetime import datetime
 from decimal import Decimal
-
-load_dotenv()
-
-# connection parameters
-connection_params = {
-    "user": os.getenv("MARIA_DB_USER"),
-    "password": os.getenv("MARIA_DB_PASSWORD"),
-    "host": os.getenv("MARIA_DB_HOST"),
-    "database": os.getenv("MARIA_DB_DB")
-}
-
-# Establish a connection
-try:
-    connection = mariadb.connect(**connection_params)
-
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-cursor = connection.cursor()
+from connector import cursor, connection
 
 with open("./data/electricity_prices.csv", mode='r', newline='') as file:
     csvFile = csv.DictReader(file)
